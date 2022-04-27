6b2ff786ee3b4c6f62cdaf9f1b3e3d5bce041bf6
Christian Tingle <ctingle18@gmail.com>
2022-04-27 16:44:52 -0400

    updated script

--- serversetup/opt/tingleauth/update_package.sh
+++ serversetup/opt/tingleauth/update_package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/bin/apt update -q -y >> /var/log/apt/automaticupdates.log
-/usr/bin/apt upgrade -q -y tingle-server-setup >> /var/log/apt/automaticupdates.log
+/usr/bin/apt install -q -y tingle-server-setup >> /var/log/apt/automaticupdates.log
